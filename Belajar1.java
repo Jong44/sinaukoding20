f364a7abd68861292567c489307c2da4eea4652f
Jong44 <mat_skak45@gmail.com>
2022-06-02 14:08:24 +0700

    Hari Pertama (bilangan prima)

--- Belajar1.java
+++ Belajar1.java
@@ -2,15 +2,30 @@ public class Belajar1 {
     public static void main(String args[]){
 
 
-      for (int n = 1; n <= 50; n++){
-          if(n % 3 == 0){
-            System.out.print(n + "habis dibagi 3");
+      //for (int n = 1; n <= 50; n++){
+          //if(n % 3 == 0){
+            //System.out.println(n + " habis dibagi 3");
+          //}
+          //else if(n % 5 == 0){
+            //System.out.println (n + " habis dibagi 5");
+          //} else {
+           // System.out.println(n);
+          //}  
+      //}
+
+      for (int n = 1; n <= 20; n++){
+        int i = 0;
+        for( int j = 1; j<=n; j++ ){
+          if (n % j== 0){
+            i++;
           }
-          else if(n % 5 == 0){
-            System.out.print(n + "habis dibagi 5");
-          } else {
-            System.out.print(n);
-          }  
+        } 
+        if (i == 2){
+          System.out.println(n + " Ini bilangan Prima");
+        } else if (i >= 2){
+          System.out.println(n + " Ini bukan bilangan Prima");
+        }
+        
       }
     }
 
