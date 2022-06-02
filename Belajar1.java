e6cd7d2808e1bd77c153eb55691b21b825ade76c
Jong44 <mat_skak45@gmail.com>
2022-06-02 11:29:15 +0700

    Hari Pertama

--- Belajar1.java
+++ Belajar1.java
@@ -1,20 +1,12 @@
 public class Belajar {
     public static void main(String args[]){
-        //int a = 1;
-        //if(a == 0){
-         //   System.out.print("A adlah 0");
-       // } else if(a == 1){
-        //    System.out.print("A adlah 1");
-      //  } else {
-       //     System.out.print("A tidak sama dengan 0 dan 1");
-      //  } 
 
 
       for (int n = 1; n <= 50; n++){
           if(n % 3 == 0){
             System.out.print(n + "habis dibagi 3");
           }
-          else if(n % 3 == 0){
+          else if(n % 5 == 0){
             System.out.print(n + "habis dibagi 5");
           }
           System.out.print(n);
